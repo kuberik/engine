ecdf9cb6c0134e1dd74afdde5b8c66dd0015652a
Luka <luka.skugor@chimeramail.com>
2020-11-25 08:46:16 +0100

    Initial migration to stable operator-sdk v1.0.0+

---
+++ api/v1alpha1/movie_types.go
@@ -0,0 +1,64 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MovieSpec defines the desired state of Movie
+type MovieSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Movie. Edit Movie_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// MovieStatus defines the observed state of Movie
+type MovieStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Movie is the Schema for the movies API
+type Movie struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MovieSpec   `json:"spec,omitempty"`
+	Status MovieStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// MovieList contains a list of Movie
+type MovieList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Movie `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Movie{}, &MovieList{})
+}
