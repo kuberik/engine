b7d292ac6740daba919d28d560dc60c7250c2c61
LittleChimera <luka.skugor@chimeramail.com>
2020-12-21 14:07:23 +0100

    Use upstream genre for generating CI Movie

--- movies/ci/docker-login.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export HOME=/kaniko
-# "the command stores credentials in $HOME/.docker/config.json"
-echo ${DOCKER_REGISTRY_PASSWORD} | docker login --username ${DOCKER_REGISTRY_USERNAME} --password-stdin ${DOCKER_REGISTRY_SERVER}
